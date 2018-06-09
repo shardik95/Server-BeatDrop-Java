8885df6c3f50c3cf77b786a823c388d32746b0fe
shardik95 <shardik95@ccs.neu.edu>
2018-06-08 21:25:23 -0400

    response received from spotify

--- BeatDrop-Server/src/main/java/com/example/BeatDropServer/services/HelloService.java
+++ BeatDrop-Server/src/main/java/com/example/BeatDropServer/services/HelloService.java
@@ -1,19 +1,49 @@
 package com.example.BeatDropServer.services;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.BeatDropServer.model.Hello;
 import com.example.BeatDropServer.repositories.HelloRepository;
 
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 @RestController
+@CrossOrigin(origins="*")
 public class HelloService {
 	@Autowired
 	HelloRepository repository;	
 	@GetMapping("/api/hello")
 	public Iterable<Hello> findAllHellos() {
 		return repository.findAll();
+	}
+	
+	@GetMapping("/api/accessToken")
+	public String getAccessToken() throws IOException {
+		OkHttpClient client = new OkHttpClient();
+
+		MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+		RequestBody body = RequestBody.create(mediaType, "grant_type=client_credentials");
+		Request request = new Request.Builder()
+		  .url("https://accounts.spotify.com/api/token")
+		  .post(body)
+		  .addHeader("Content-Type", "application/x-www-form-urlencoded")
+		  .addHeader("Authorization", "Basic MWFkYTcxMmFkODlkNDQ4Mjg2ZjhkODkyODY4NjIxZDU6MTg3NDE0MmVhZTQ5NDhiZWI3YTRjMzVkYmIyMjZkYzU=")
+		  .addHeader("Cache-Control", "no-cache")
+		  .build();
+
+		Response response = client.newCall(request).execute();
+		return response.body().string();
 	}
 }
 
