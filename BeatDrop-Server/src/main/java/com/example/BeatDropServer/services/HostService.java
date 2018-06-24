8757411288f112588c4c649129ff90937624e8a2
shardik95 <shardik95@ccs.neu.edu>
2018-06-24 04:35:07 -0400

    admin for host done

--- BeatDrop-Server/src/main/java/com/example/BeatDropServer/services/HostService.java
+++ BeatDrop-Server/src/main/java/com/example/BeatDropServer/services/HostService.java
@@ -6,12 +6,16 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.BeatDropServer.model.Host;
-
+import com.example.BeatDropServer.model.User;
 import com.example.BeatDropServer.repositories.HostRepository;
 
 @RestController
@@ -35,6 +39,33 @@ public class HostService {
 	
 	public Host createUser(@RequestBody Host user) {
 		return hostRepository.save(user);
+	}
+	
+	@GetMapping("/api/host")
+	public List<Host> getAllHosts(){
+		return (List<Host>) hostRepository.findAll();
+	}
+	
+	@DeleteMapping("/api/host/{hostId}")
+	public int deleteHost(@PathVariable("hostId") int hostId) {
+		hostRepository.deleteById(hostId);
+		return 1;
+	}
+	
+	@PutMapping("/api/host")
+	public User updateUser(@RequestBody Host newUser) {
+		List<Host> users=(List<Host>) hostRepository.findHostByUserName(newUser.getUserName());
+		if(users.size()>0) {
+			Host user=users.get(0);
+			user.setFirstName(newUser.getFirstName());
+			user.setLastName(newUser.getLastName());
+			user.setPhone(newUser.getPhone());
+			user.setDob(newUser.getDob());
+			user.setPassword(newUser.getPassword());
+			hostRepository.save(user);
+			return user;
+		}
+		return null;
 	}
 	
 }
